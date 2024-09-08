7e729d5775ee8300544769902898194050c8612c
stonoy <sowrabh2013@gmail.com>
2024-09-08 17:25:15 +0530

    urls extraction by recurtion

--- main.go
+++ main.go
@@ -3,22 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(getURLsFromHTML(`
-		<html>
-			<body>
-				<a href="/path/one#thearea">
-					<span>Boot.dev</span>
-				</a>
-				<a href="https://other.com/path/one/search?q=any#requiredarea">
-					<span>Boot.dev</span>
-				</a>
-				<section>
-					<div>
-						<img src="any/where/in/web" />
-					</div>
-				</section>
-			</body>
-		</html>
-	`, "https://blog.boot.dev"))
+	fmt.Println("Welcome!")
 
 }
